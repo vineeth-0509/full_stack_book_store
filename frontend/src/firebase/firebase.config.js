98ef987413e8568cacfbd3b973e2313ff305f370
vineeth Thungani <152182581+vineeth-0509@users.noreply.github.com>
2025-04-10 09:43:59 +0530

    Update firebase.config.js

--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -6,13 +6,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCQbg_5lBae2H8Tf11WUonuiVYyEQ7xmoA",
-  authDomain: "abiding-circle-448421-c6.firebaseapp.com",
-  projectId: "abiding-circle-448421-c6",
-  storageBucket: "abiding-circle-448421-c6.firebasestorage.app",
-  messagingSenderId: "141090333016",
-  appId: "1:141090333016:web:dd943d67b2dca64945c52d",
-  measurementId: "G-VVCXCHP2CP",
+
 };
 
 // Initialize Firebase
